525280c1294a7add6e8304f656743377be035cc5
sai241194 <143655417+sai241194@users.noreply.github.com>
2024-01-17 16:01:44 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "saibucket14012024" # Replace with your actual S3 bucket name
+    bucket = "saibucket17012024" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
